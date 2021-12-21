4c9988d7e73f3374c0a9b1177ea4a1522566075b
mintyv <vangm29@gmail.com>
2021-12-21 00:11:58 -0600

    changes to new project

--- First-Project/src/main/java/HelloWorld/Helloworld.java
+++ First-Project/src/main/java/HelloWorld/Helloworld.java
@@ -3,8 +3,11 @@ package HelloWorld;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
 }
+
+
+
